05ded3ad8b23067182d2157faa8f3749668ceaad
ishmelev <ishmelev@1ditis.ru>
2018-06-24 21:01:46 +0300

    24.06.18

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var pug = require('gulp-pug');
 var path = {
         build: { //Тут мы укажем куда складывать готовые после сборки файлы
-            html: './build/../',
-            js: './build/../',
-            css: '/build/../',
+            html: '.',
+            js: './build',
+            css: './build',
             img: './build/img/',
             fonts: './build/fonts/'
         },
         src: { //Пути откуда брать исходники
-            html: 'src/**/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+            pug: 'src/*.pug', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
             js: 'src/**/*.js',//В стилях и скриптах нам понадобятся только main файлы
             sass: 'src/**/*.scss',
             img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
@@ -24,20 +25,30 @@ var path = {
         },
     };
 
-gulp.task('pug:html', function(){
-    gulp.src(path.src.html).pipe(gulp.dest(path.build.html));
-    return gulp.src(dest.src.style);
+gulp.task('pug:html', function buildHTML() {
+    return gulp.src(path.src.pug)
+        .pipe(pug({
+                    pretty: true
+                }))
+        .pipe(gulp.dest(path.build.html)); // указываем gulp куда положить скомпилированные HTML файлы
 });
 
-gulp.task('sass', function(){
-    gulp.src(path.src.sass).pipe(gulp.dest(path.build.css));
-    return gulp.src(path.build.sass);    
+gulp.task('sass:css', function () {
+    gulp.src(path.src.sass)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(path.build.css));
+});
+
+gulp.task('js', function(){
+    gulp.src(path.src.js)
+        .pipe(gulp.dest(path.build.js));
+    return gulp.src(dest.src.js);
 });
 
 gulp.task('watch', function(){
-    gulp.watch(path.src.html);
-    gulp.watch(path.src.sass);
-    gulp.watch(path.src.js);
+    gulp.watch(path.src.pug, ['pug:html']);
+    gulp.watch(path.src.sass, ['sass:css']);
+    gulp.watch(path.src.js, ['js']);
     gulp.watch(path.src.img);
     gulp.watch(path.src.fonts);            
 });
